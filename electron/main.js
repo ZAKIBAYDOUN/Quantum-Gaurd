045b3a4c4afb3e8803beb156a2abc3b87f75f835
547atoshinakamo70 <ramibaydoun@proton.me>
2025-08-14 19:51:16 +0200

    build: add electron packaging; init dapp

--- electron/main.js
+++ electron/main.js
@@ -1,15 +1,9 @@
 ﻿const { app, BrowserWindow } = require("electron");
 const path = require("path");
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1200, height: 800,
-    webPreferences: { contextIsolation: true }
-  });
+  const win = new BrowserWindow({ width: 1200, height: 800, webPreferences: { contextIsolation: true } });
   win.removeMenu?.();
   win.loadFile(path.join(__dirname, "../dapp/index.html"));
 }
-app.whenReady().then(() => {
-  createWindow();
-  app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
-});
+app.whenReady().then(() => { createWindow(); app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); }); });
 app.on("window-all-closed", () => { if (process.platform !== "darwin") app.quit(); });
